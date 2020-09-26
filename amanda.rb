ed4f5d078a6478b27a87a3a1389ae6ff7e468442
Matthew Sallin <msallin@gmail.com>
2020-09-26 21:48:00 +0000

    Automatically backed up by Learn

--- amanda.rb
+++ amanda.rb
@@ -1,13 +1,16 @@
 #Results in [you? are How] instead of [woH era ?uoy], but [collect] works
 def reverse_each_word(string)
   returnedArray = []
-  newArray = string.split(" ")
-  newArray = newArray.reverse
+  newArray = string.split(" ") # = ["How", "are", "you?"]
+  #puts "newArray = #{newArray}"
+  newArray = newArray.reverse # = ["you?", "are", "How"]
+  #puts "newArray = #{newArray}"
   newArray.collect do |word|
-    returnedArray << word.reverse
+    returnedArray << word.reverse # = builds an array of reverse words ["?uoy"] --> ["?uoy", "era"] --> ["?uoy", "era", "woH"]
+    #puts "returnedArray = #{returnedArray}"
   end
-  newString = returnedArray.join(" ").to_s
-  puts "newString = #{newString}"
+  newString = returnedArray.join(" ").to_s # = ?uoy era woH
+  #puts "newString = #{newString}"
 end
 
 reverse_each_word("How are you?")
